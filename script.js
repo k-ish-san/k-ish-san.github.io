992294bbb3172927ad49c5456bf78362922dbb51
Samrudh Kishsan P M <126341963+k-ish-san@users.noreply.github.com>
2024-03-30 13:10:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Dark mode toggle
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
+// Menu button toggle
+function toggleMenu() {
+    var menu = document.querySelector('nav ul');
+    menu.classList.toggle('show');
+}
+
+// Event listeners
+document.querySelector('.menu-btn').addEventListener('click', toggleMenu);
+document.querySelector('.dark-mode-toggle').addEventListener('click', toggleDarkMode);
 
